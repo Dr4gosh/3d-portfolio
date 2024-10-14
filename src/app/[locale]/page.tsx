41623efa9aaf3ecec3dc3d570eba6bf694e62855
Dragos <Dr4gosh_97@yahoo.com>
2024-10-14 22:28:00 +0300

    feat: made i18n work

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,10 @@
 import Hero from "@/components/Hero";
 import ParticlesContainer from "@/components/ParticlesContainer";
-
+import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
 
 export default function Home() {
+  const t = useTranslations("HomePage");
   return (
     <main className="relative w-full bg-hero-pattern bg-cover bg-center bg-no-repeat">
       {/* <ParticlesContainer /> */}
@@ -14,6 +15,10 @@ export default function Home() {
       >
         Salut2
       </Link>
+      <div className="h-[600px] w-[1000px] bg-black text-white">
+        <p>{t("title")}</p>
+      </div>
+
       {/* <Hero /> */}
     </main>
   );
